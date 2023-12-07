e4d02129ddcfa61c15cf72937687bec5c19fdc70
Pamela Viale <pamelaviale87@gmail.com>
2023-12-07 12:15:30 -0300

    Create a function to hide filters

--- java-script/main.js
+++ java-script/main.js
@@ -1,3 +1,11 @@
 const $ = (selector) => document.querySelector(selector)
+const $$ = (selector) => document.querySelector(selector)
 
 
+//Ocultar filtros 
+
+$("#hideFilters").addEventListener("click", (e)=>{
+    e.preventDefault()
+    $("#hidden-filter-type").style.display = $("#hidden-filter-type").style.display === "none" ? "block" : "none" 
+
+})
